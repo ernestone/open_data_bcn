b9999edc8e3d3d26e9a37565d10a077ec242c002
Ernesto Arredondo Martinez <ernestone@gmail.com>
2018-04-21 12:21:32 +0200

    - Added anaconda-project- Added .gitignore- Removed dirs checkpoints notebooks

--- .ipynb_checkpoints/start_mi_jupyter_open_data_bcn-checkpoint.sh
+++
@@ -1,1 +0,0 @@
-docker run -it --rm --name jupyter_bcn_data -p 8888:8888 -v /Users/ernestoarredondomartinez/Dev/GITHUB/My_Repos/Open_data_Bcn:/home/jovyan/work jupyter/base-notebook start.sh jupyter lab
